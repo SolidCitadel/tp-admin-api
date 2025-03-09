a569baf8f1e8f9c97539c70ab9116f87cff2be8c
HyeonJun Jeon <teddy3582@naver.com>
2025-03-09 19:32:30 +0900

    fix: tailwind watch working

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit',
+    // mode: 'jit',
     content: ["../resources/templates/**/*.{html,js}"],
     theme: {
         extend: {},
